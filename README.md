60f12fb64eb4168c997ea2362383746c502557b9
Your Name <julioc255io@gmail.com>
2015-07-09 12:00:43 -0500

    New version

--- README.md
+++ README.md
@@ -1,1 +1,141 @@
 # CRUD-js-localStorage
+
+CRUD en Javascript utilizando localStorage de la charla sobre Javascript en [http://slides.com/juliocesar_io/html5-forms-localstorage](http://slides.com/juliocesar_io/html5-forms-localstorage)
+
+Presentaciones anteriores:
+
+- Introducción a Javascript      [http://slides.com/juliocesar_io/intro-js](http://slides.com/juliocesar_io/intro-js)
+
+- Jquery y Javascript [http://slides.com/juliocesar_io/js-2](http://slides.com/juliocesar_io/js-2)
+
+
+### Creación del localStorage
+
+Debemos crear una instancia en el localStorage donde se guardarán literalmente un **string**. Para poder hacer el intercambio entre objeto **JSON** y string usaremos las funciones `stringify` y `parse` y en un array tendremos disponibles siempre la colección del los objetos para poderlos iterar.
+
+
+```
+var dbVacas = localStorage.getItem("dbVacas"); //Obtener datos de localStorage
+dbVacas = JSON.parse(dbVacas); // Covertir a objeto
+if (dbVacas === null) // Si no existe, creamos un array vacio.
+    dbVacas = [];
+```
+
+
+### Funciones
+
+En el script `funciones.js` definimos una función o metetodo para cada uno de los casos:
+
+- Creación de un objeto
+- Actualizción
+- Eliminación
+
+Tambien funciones de consulta como listado de los atributos y algo para contar el total de objetos.
+
+Para poder manejar estas acciones usamos un modo de operación como un `flag` asi,
+
+```
+var operacion = "A"; //"A"=agregar; "E"=edtidar
+
+```
+
+Luego esperamos por un evento usando Jquery y mandamos a la funcion en cuestión.
+```
+// Esperar el evento de envio del formulario !!
+$("#vacas-form").bind("submit", function() {
+    debugger;
+    if (operacion == "A")
+        return AgregarVaca();
+    else {
+        return Editar();
+    }
+});
+```
+
+
+
+### Ejercicios
+
+- Aclaración sobre comparadores lógicos
+
+```
+0 == false;   // true
+0 === false;  // false, por que son de tipo diferente
+1 == "1";     // true, Coversión automatica solamente para el valor
+1 === "1";    // false, Son de tipo diferente
+null == undefined; // true
+null === undefined; // false
+'0' == false; // true
+'0' === false; // false
+```
+
+> Escribir un bucle que va desde x = 0 hasta x <10.
+> Dentro del bucle, si x es par,  mostrar x y "par" en la consola.
+si x es impar, mostrar X e "impar" en la consola.
+
+```
+
+for (var i = 0; i < 10; i++) {
+    if (i % 2 === 0 ) {
+        console.log(i, "Es par");
+    }else{
+        console.log(i, "Es impar");
+    }
+}
+```
+
+> Objeto Javascript
+
+```
+
+var SemilleroSecbi = {
+  url: "http://secbi.co",
+
+  integrantes: {
+    primernombre: "Ivan",
+    segundoNombre: "Ortiz"
+  },
+
+  lineasInvestigacion:{
+    nombre: "Ing.Software",
+    publicaciones: "2"
+  }
+};
+
+```
+> Más de un integrante ? Un objeto dentro de un objeto? , usamos arrays.
+
+```
+var SemilleroSecbi = {
+  url: "http://secbi.co",
+
+  integrantes: [
+    {
+      primerNombre: "Ivan",
+      segundoNombre: "Ortiz"
+    },
+    {
+      primerNombre: "Alguien",
+      segundoNombre: "Mas"
+    }
+  ]
+};
+
+```
+> Operaciones con arrays
+
+```
+// Array Original
+var array = ["uno", "dos", "cuatro"];
+// splice(position, numberOfItemsToRemove, item)
+array.splice(2, 0, "tres");
+
+array;  // ["uno", "dos", "tres", "cuatro"]
+
+```
+
+> Como limpiar el localStorage ?
+
+```
+localStorage.clear();
+```
--- funciones.js
+++ funciones.js
@@ -1,27 +1,156 @@
-// Definan las variables globales
-var dbPerritos = localStorage.getItem("dbPerritos");
-dbPerritos = JSON.parse(dbPerritos);
-if (dbPerritos === null)
-    dbPerritos = [];
 
-function agregarPerrito() {
-    var datoCliente = JSON.stringify({
-        nombre: $("#nombre").val(),
-        genero: $("input[name='genero']:checked", "#perritos-form").val(),
-        fecha_nacimiento: $("#fecha_nacimiento").val(),
-        color: $("#color").val(),
-        raza: $("#raza").val(),
-        codigo: $("#codigo").val()
+var dbVacas = localStorage.getItem("dbVacas"); //Obtener datos de localStorage
+var operacion = "A"; //"A"=agregar; "E"=edtidar
+dbVacas = JSON.parse(dbVacas); // Covertir a objeto
+if (dbVacas === null) // Si no existe, creamos un array vacio.
+    dbVacas = [];
+
+
+function Mensaje(t){
+        switch (t) {
+            case 1: //
+                $(".mensaje-alerta").append(
+                    "<div class='alert alert-success' role='alert'>Se agrego con exito la vaca</div>"
+                );
+                break;
+            case 2: //
+                $(".mensaje-alerta").append(
+                    "<div class='alert alert-danger' role='alert'>Se elimino la vaca</div>"
+                );
+                break;
+            default:
+
+        }
+    }
+
+
+function AgregarVaca () {
+    // Seleccionamos los datos de los inputs de formulario
+    var datos_cliente = JSON.stringify({
+        Nombre : $("#nombre").val(),
+        Correo : $("#correo").val(),
+        Peso : $("#peso").val(),
+        Fecha_nacimiento : $("#fecha_nacimiento").val(),
     });
 
-    dbPerritos.push(datoCliente);
-    localStorage.setItem("dbPerritos", JSON.stringify(dbPerritos));
-    alert("Se guardo tu perrito con exito");
-    return true;
+    dbVacas.push(datos_cliente); // Guardar datos en el array definido globalmente
+    localStorage.setItem("dbVacas", JSON.stringify(dbVacas));
+
+
+
+    ListarVacas();
+
+
+    return Mensaje(1);
 }
 
 
-// Esperamos por el evento
-$("#perritos-form").submit(function () {
-    agregarPerrito();
+
+function ListarVacas (){
+    $("#dbVacas-list").html(
+            "<thead>" +
+                "<tr>" +
+                    "<th> ID </th>" +
+
+                    "<th> Nombre </th>" +
+                    "<th> Correo </th>" +
+                    "<th> Peso </th>" +
+                    "<th> fecha_nacimiento </th>" +
+                    "<th> </th>" +
+                    "<th>  </th>" +
+                "</tr>" +
+            "</thead>" +
+            "<tbody>" +
+            "</tbody>"
+    );
+
+    for (var i in dbVacas) {
+        var d = JSON.parse(dbVacas[i]);
+        $("#dbVacas-list").append(
+                        "<tr>" +
+                            "<td>" + i + "</td>" +
+                            "<td>" + d.Nombre + "</td>" +
+                            "<td>" + d.Correo + "</td>" +
+                            "<td>" + d.Peso + "</td>" +
+                            "<td>" + d.Fecha_nacimiento + "</td>" +
+                            "<td> <a id='"+ i +"' class='btnEditar' href='#'> <span class='glyphicon glyphicon-pencil'> </span>  </a> </td>" +
+                            "<td> <a id='" + i + "' class='btnEliminar' href='#'> <span class='glyphicon glyphicon-trash'> </span> </a> </td>" +
+                        "</tr>"
+                           );
+    }
+
+}
+
+
+if (dbVacas.length !== 0) {
+    ListarVacas();
+} else {
+    $("#dbVacas-list").append("<h2> No tienes vacas </h2>");
+}
+
+function contarVacas(){
+    var vacas = dbVacas;
+    nVacas = vacas.length;
+
+    $("#numeroVacas").append(
+        "<a>Tienes actualmente" + "<br>" + "<span class='badge'>" + nVacas + "</span></a> Vacas"
+    );
+    return nVacas;
+}
+
+function Eliminar(e){
+    dbVacas.splice(e, 1); // Args (posición en el array, numero de items a eliminar)
+    localStorage.setItem("dbVacas", JSON.stringify(dbVacas));
+    return Mensaje(2);
+}
+
+function Editar() {
+    dbVacas[indice_selecionado] = JSON.stringify({
+        Nombre : $("#nombre").val(),
+        Correo : $("#correo").val(),
+        Peso : $("#peso").val(),
+        Fecha_nacimiento : $("#fecha_nacimiento").val(),
+    });
+    localStorage.setItem("dbVacas", JSON.stringify(dbVacas));
+    operacion = "A"; //Regresamos la valor original
+    return true;
+
+}
+
+$(".btnEliminar").bind("click", function(){
+    alert("¿ Me quieres eliminar ?");
+    indice_selecionado = $(this).attr("id"); // "this" contiene el elemento clikeado en el contexto actual
+    console.log(indice_selecionado);
+    console.log(this);
+    Eliminar(indice_selecionado); // Eliminamos el elemento llamando la funcion de eliminar
+    ListarVacas();
+});
+
+$(".btnEditar").bind("click", function() {
+    alert("¿ Me quieres editar ?");
+    // Cambiamos el modo
+    $(".modo").html("<span class='glyphicon glyphicon-pencil'> </span> Modo edición");
+    operacion = "E";
+    indice_selecionado = $(this).attr("id");
+    console.log(indice_selecionado);
+    console.log(this);
+    // Llemanos el formulario con los datos actuales de la vaca a editar
+    var vacaItem = JSON.parse(dbVacas[indice_selecionado]);
+    $("#nombre").val(vacaItem.Nombre);
+    $("#correo").val(vacaItem.Correo);
+    $("#peso").val(vacaItem.Peso);
+    $("#fecha_nacimiento").val(vacaItem.Fecha_nacimiento);
+    $("#nombre").focus();
+});
+
+
+contarVacas();
+// Esperar el evento de envio del formulario !!
+$("#vacas-form").bind("submit", function() {
+    debugger;
+    if (operacion == "A")
+        return AgregarVaca();
+    else {
+        return Editar();
+    }
 });
